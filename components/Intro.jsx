230d4bdb1178ef5287d81c5cdcb2fc0749c975dd
Pankaj <pankaj92999@gmail.com>
2025-11-10 17:50:18 +0530

    addon style changes

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -2,7 +2,7 @@ const Intro = ({ name }) => {
   return (
     <section className="intro p-0 relative md:-top-15">
       <div className="container">
-        <div className="intro">
+        <div className="intro max-w-[435px]">
           <h2 className="text-marigold text-3xl mb-3 font-helvetica-neue-bold">{name}</h2>
         </div>
       </div>
