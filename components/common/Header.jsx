6c9da97e2adf8d601ff86f5ad9d8ae90661a5e9c
Pankaj Kumar <pankaj@webenertia.com>
2025-12-10 00:01:33 +0530

    financing page changes

--- components/common/Header.jsx
+++ components/common/Header.jsx
@@ -49,12 +49,12 @@ const Header = () => {
                   </Link>
                 </li>
                 <li className="button-text text-lg">
-                  <a
+                  <Link
                     className="text-marigold text-base font-normal hover:text-dark-teal hover:underline hover:decoration-marigold hover:underline-offset-5"
-                    href="#"
+                    to="/financing"
                   >
                     Financing
-                  </a>
+                  </Link>
                 </li>
                 <li className="button-text text-lg">
                   <a
